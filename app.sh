2cc3e3a67fd054305bc37dfa2ba4823111dd7d84
Student 9 <student9@example.com>
2026-09-10 10:06:38 +0000

    Add workshop application v1.0

---
+++ app.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "======================================"
+echo "     GITOPS WORKSHOP APPLICATION"
+echo "======================================"
+echo ""
+echo "Version: 1.0"
+echo "Participant: Student 9"
+echo ""
+echo "Application test passed."
